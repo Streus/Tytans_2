0831838b70d3fe1271263a2fb1f5aea3eab444d3
Streusiladei Malieavek <embersteelstreus@gmail.com>
2016-08-29 15:59:53 -0500

    Additionakl Front-end work

---
+++ Assets/Resources/Scripts/Management/UI/OpeningMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpeningMenu : MonoBehaviour {
+
+	public float dispDuration;
+	public MenuManager manager;
+	public Menu transitionMenu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		dispDuration -= Time.deltaTime;
+		if (dispDuration <= 0f) {
+			manager.showMenu (transitionMenu);
+			Destroy (transform.gameObject);
+		}
+	}
+}
