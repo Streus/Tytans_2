ed03f7d8ddae72f5a832c1bfc2d98f34222d2308
Streusiladei Malieavek <embersteelstreus@gmail.com>
2016-08-31 00:37:04 -0500

    Work on New Game menu, Options/Controls Animation, Sprites

--- Assets/Resources/Scripts/Management/GameManager.cs
+++ Assets/Resources/Scripts/Management/GameManager.cs
@@ -3,9 +3,22 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+	// The one game-manager
+	public static GameManager manager;
+
+	// The player object
+	public static GameObject player;
+
 	// Use this for initialization
 	void Start () {
-		DontDestroyOnLoad (transform.gameObject);
+		// ensure there's only one GameManager
+		if(manager == null){
+			DontDestroyOnLoad (transform.gameObject);
+			manager = this;
+		}
+		else if(manager != this){
+			Destroy(transform.gameObject);
+		}
 	}
 	
 	// Update is called once per frame
--- Assets/Resources/Scripts/Management/UI/NewGameCreator.cs
+++ Assets/Resources/Scripts/Management/UI/NewGameCreator.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class NewGameCreator : MonoBehaviour {
 
@@ -11,6 +12,7 @@ public class NewGameCreator : MonoBehaviour {
 
 	// Player options
 	private PlayerClass pClass = PlayerClass.defender;
+	private Sprite shipSprite;
 	private float healthMax = 0;
 	private float healthRegen = 0;
 	private float energyMax = 0;
@@ -19,18 +21,50 @@ public class NewGameCreator : MonoBehaviour {
 	private string classAbility = "Dash";
 	private string shootAbility = "BasicShot";
 	private GameObject bulletType;
-	private Sprite shipSprite;
+
+	// Important children
+	Transform classGroup;
+	Text classDescription;
+	Transform bulletGroup;
 
 	// Use this for initialization
 	void Start () {
-	
+		classGroup = transform.GetChild(0).GetChild(2);
+		classDescription = transform.GetChild(0).GetChild(3).GetChild(0).GetComponent<Text>();
+		bulletGroup = transform.GetChild(0).GetChild(7);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
-}
+
+	// All setters
+	public void setDifficulty(int x) { difficulty = (Difficulty)x; }
+
+	public void setClass(int pc)
+	{
+		pClass = (PlayerClass)pc;
+		switch(pClass){
+		case PlayerClass.defender:
+			classDescription.text = 
+				"Defender.  Slow but sturdy front-line ship that thrives when under fire.";
+			break;
+		case PlayerClass.skirmisher:
+			classDescription.text = 
+				"Skirmisher.  Fast and agile fighter that survives by avoiding hits.";
+			break;
+		case PlayerClass.caster:
+			classDescription.text = 
+				"Obliterator.  Advanced craft that, while fragile, has absurd destructive capability.";
+			break;
+		}
+	}
+
+	public GameObject getBullet() { return bulletType; }
+	public void setBullet(GameObject x) { bulletType = x; }
+		
+	} 
 
 public enum PlayerClass
 {
