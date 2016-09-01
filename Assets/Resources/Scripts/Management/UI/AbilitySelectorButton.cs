8902dfc298323549e80d27d26555b135832b5689
Streusiladei Malieavek <embersteelstreus@gmail.com>
2016-09-01 01:18:00 -0500

    Added Inventory

---
+++ Assets/Resources/Scripts/Management/UI/AbilitySelectorButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AbilitySelectorButton : MonoBehaviour {
+
+	private Image abilityGraphic;
+	private GameObject slotList;
+	public Ability slotValue;
+
+	// Use this for initialization
+	void Start () {
+		//hide the dropdown
+		slotList = transform.GetChild(0).gameObject;
+		slotList.SetActive(false);
+
+		//set the graphic to the given ability, if there is one.
+		abilityGraphic = transform.GetChild(1).GetComponent<Image>();
+		if(slotValue != null)
+			abilityGraphic.sprite = slotValue.image;
+		else
+			abilityGraphic.color = Color.clear;
+			
+	}
+
+	public void toggleListActive()
+	{
+		slotList.SetActive(!slotList.activeSelf);
+	}
+}
---
+++ Assets/Resources/Scripts/Management/UI/InventoryButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class InventoryButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	private Animator invAnim;
+
+	// Use this for initialization
+	void Start () {
+		invAnim = transform.parent.GetComponent<Animator>();
+	}
+
+	public void OnPointerEnter(PointerEventData eventData){
+		invAnim.SetBool("Peek", true);
+	}
+
+	public void OnPointerExit(PointerEventData eventData){
+		invAnim.SetBool("Peek", false);
+	}
+
+	public void openInventory(){
+		invAnim.SetBool("IsOpen", !invAnim.GetBool("IsOpen"));
+	}
+}
