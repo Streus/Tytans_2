fcd0aac308df2187f11a0f72ed15e5f5d068f317
Streusiladei Malieavek <embersteelstreus@gmail.com>
2016-09-22 11:28:15 -0500

    Finished mouseover info.  Added health and energy pickups.

---
+++ Assets/Resources/Scripts/Game/World Objects/Interactable/HealthPickUp.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthPickUp : MonoBehaviour {
+
+	private Rigidbody2D physbody;
+
+	void Awake () {
+		physbody = transform.GetComponent<Rigidbody2D> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//just for effect
+		transform.rotation = Quaternion.Euler(new Vector3(0, 0, transform.rotation.eulerAngles.z + 1));
+	}
+
+	void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject == GameManager.player) 
+		{
+			col.gameObject.GetComponent<Entity> ().health += 20f;
+			Destroy (gameObject);
+		}
+	}
+}
