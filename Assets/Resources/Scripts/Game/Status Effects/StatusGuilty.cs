92307db1fb62daed240e980453e5cee83659dd5a
Streusiladei Malieavek <embersteelstreus@gmail.com>
2016-10-13 16:25:43 -0500

    Added Guilty and Innocent status effects

---
+++ Assets/Resources/Scripts/Game/Status Effects/StatusGuilty.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatusGuilty : StatusEffect {
+
+	private float armorReduction;
+
+	public StatusGuilty(float dur, Transform e, float ar) : base(dur, e)
+	{
+		name = "Guilty";
+		desc = "Taking additional damage";
+		icon = Resources.Load<Sprite> ("Sprites/UI/Status Effects/StatusEffectGuilty");
+
+		armorReduction = ar;
+	}
+
+	public override void apply ()
+	{
+		//do effect
+
+
+		//apply debuff
+		invokerVars.armor -= armorReduction;
+	}
+
+	public override void revert ()
+	{
+		invokerVars.armor += armorReduction;
+	}
+}
---
+++ Assets/Resources/Scripts/Game/Status Effects/StatusInnocent.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatusInnocent : StatusEffect {
+
+	private float regenAmount;
+
+	public StatusInnocent(float dur, Transform e, float ra) : base(dur, e)
+	{
+		name = "Innocent";
+		desc = "Regenerating Health and Energy";
+		icon = Resources.Load<Sprite> ("Sprites/UI/Status Effects/StatusEffectInnocent");
+
+		regenAmount = ra;
+	}
+
+	public override void apply ()
+	{
+		//add effect
+
+
+		//add buffs
+		invokerVars.healthRegen += regenAmount;
+		invokerVars.energyRegen += regenAmount;
+	}
+
+	public override void revert ()
+	{
+		//remove effect
+
+
+		//remove buffs
+		invokerVars.healthRegen -= regenAmount;
+		invokerVars.energyRegen -= regenAmount;
+	}
+}
